c9bf3ac7f236822a436ae4be9167bd0d2c7a6060
Tortuga <kmm2459395@stu.o-hara.ac.jp>
2025-10-29 11:29:20 +0900

    a

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello,world.You're at polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
